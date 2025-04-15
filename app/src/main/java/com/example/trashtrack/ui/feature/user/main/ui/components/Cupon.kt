d45c18f2d1f092a12197db13db738c5429d6075d
Kirill-wq333 <154874212+Kirill-wq333@users.noreply.github.com>
2025-04-15 20:03:22 +0400

    Added new functions

--- app/src/main/java/com/example/trashtrack/ui/feature/user/main/ui/components/Cupon.kt
+++ app/src/main/java/com/example/trashtrack/ui/feature/user/main/ui/components/Cupon.kt
@@ -1,10 +1,38 @@
 package com.example.trashtrack.ui.feature.user.main.ui.components
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.trashtrack.R
+import com.example.trashtrack.ui.theme.TTTypography
+import com.example.trashtrack.ui.theme.colors
+
+@Preview
+@Composable
+private fun CuponPreview() {
+    Surface {
+        Cupon()
+    }
+}
 
 @Composable
 fun Cupon(
@@ -13,6 +41,94 @@ fun Cupon(
     Box(
         modifier = Modifier
             .fillMaxWidth()
-            .padding()
-    )
+            .padding(start = 39.dp, end = 25.dp)
+            .background(color = MaterialTheme.colors.green600, shape = RoundedCornerShape(12.dp))
+    ){
+        TrashBags()
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(
+                    start = 12.dp,
+                    bottom = 13.dp,
+                    end = 25.dp,
+                    top = 15.dp
+                ),
+            verticalArrangement = Arrangement.spacedBy(12.59.dp)
+        ) {
+            Text(
+                text = "Если оплатите на год вынос мусора",
+                color = MaterialTheme.colors.white,
+                style = TTTypography.headlineLarge
+            )
+            Box(
+                modifier = Modifier
+                    .border(width = 3.dp, color = MaterialTheme.colors.white, shape = RoundedCornerShape(32.dp))
+            ) {
+                Text(
+                    text = "Будет скидка 30%",
+                    color = MaterialTheme.colors.white,
+                    style = TTTypography.titleLarge,
+                    modifier = Modifier
+                        .padding(
+                            top = 10.dp,
+                            start = 10.dp,
+                            bottom = 10.dp,
+                            end = 22.dp
+                        )
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun TrashBags(
+    modifier: Modifier = Modifier
+) {
+    val tint = MaterialTheme.colors.white.copy(0.45f)
+
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+    ){
+        Icon(
+            imageVector = ImageVector.vectorResource(R.drawable.streamline_bag),
+            contentDescription = null,
+            tint = tint,
+            modifier = Modifier
+                .offset(y = 34.6.dp)
+                .size(71.6.dp)
+                .rotate(60f)
+        )
+        Icon(
+            imageVector = ImageVector.vectorResource(R.drawable.streamline_bag),
+            contentDescription = null,
+            tint = tint,
+            modifier = Modifier
+                .offset(y = -(10).dp)
+                .size(71.6.dp)
+                .align(Alignment.TopEnd)
+                .rotate(60f)
+        )
+        Icon(
+            imageVector = ImageVector.vectorResource(R.drawable.streamline_bag),
+            contentDescription = null,
+            tint = tint,
+            modifier = Modifier
+                .offset(y = 32.4.dp, x = -(31.4).dp)
+                .size(71.6.dp)
+                .align(Alignment.TopEnd)
+        )
+        Icon(
+            imageVector = ImageVector.vectorResource(R.drawable.streamline_bag),
+            contentDescription = null,
+            tint = tint,
+            modifier = Modifier
+                .offset(y = 70.dp)
+                .size(71.6.dp)
+                .align(Alignment.TopEnd)
+                .rotate(60f)
+        )
+    }
 }
--- app/src/main/java/com/example/trashtrack/ui/feature/user/main/ui/components/PlaceAnOrder.kt
+++ app/src/main/java/com/example/trashtrack/ui/feature/user/main/ui/components/PlaceAnOrder.kt
@@ -1,8 +1,50 @@
 package com.example.trashtrack.ui.feature.user.main.ui.components
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.trashtrack.ui.theme.TTTypography
+import com.example.trashtrack.ui.theme.colors
 
 @Composable
 fun PlaceAnOrder() {
-
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 39.dp, end = 25.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(20.dp)
+    ) {
+        Text(
+            text = "Вывезим мусор из жилых помещений за вас",
+            color = MaterialTheme.colors.black,
+            style = TTTypography.headlineLarge
+        )
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(
+                    color = MaterialTheme.colors.green600,
+                    shape = RoundedCornerShape(12.dp)
+                    )
+        ){
+            Text(
+                text = "Сделать заказ",
+                color = MaterialTheme.colors.white,
+                style = TTTypography.headlineLarge,
+                modifier = Modifier
+                    .padding(vertical = 13.dp)
+            )
+        }
+    }
 }
