b30dc5ad175bdc6b8d8dc0f6572e777ab2df4e78
KirillSharonov <154874212+Kirill-wq333@users.noreply.github.com>
2025-04-21 13:52:07 +0400

    Added new functions and .xml painters

---
+++ app/src/main/java/com/example/trashtrack/ui/feature/user/profile/bottomsheet/DeleteAccount.kt
@@ -0,0 +1,74 @@
+package com.example.trashtrack.ui.feature.user.profile.bottomsheet
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import com.example.trashtrack.ui.preferences.AuthManager
+import com.example.trashtrack.ui.shared.button.TTBottom
+import com.example.trashtrack.ui.theme.TTTypography
+import com.example.trashtrack.ui.theme.colors
+
+@Composable
+fun DeleteAccount(
+    modifier: Modifier = Modifier,
+    openProfileScreen: () -> Unit,
+    openIntroductionScreen: () -> Unit
+) {
+    val context = LocalContext.current
+
+    val authManager = remember { AuthManager.getInstance(context) }
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 22.dp, end = 22.dp, bottom = 37.dp)
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth(),
+            verticalArrangement = Arrangement.spacedBy(21.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "Удаление аккаунта",
+                color = MaterialTheme.colors.neutral950,
+                style = TTTypography.headlineLarge
+            )
+
+            Text(
+                text = "Вы уверены, что хотите удалить аккаунт?",
+                color = MaterialTheme.colors.neutral500,
+                style = TTTypography.titleLarge
+            )
+        }
+        Spacer(modifier = Modifier.height(49.dp))
+        Column(
+            modifier = Modifier
+                .fillMaxWidth(),
+            verticalArrangement = Arrangement.spacedBy(11.dp)
+        ) {
+            TTBottom(
+                text = "Да",
+                onClick = { authManager.deleteAccount(); openIntroductionScreen() },
+                color = MaterialTheme.colors.green600,
+            )
+            TTBottom(
+                text = "Отмена",
+                onClick = openProfileScreen,
+                color = MaterialTheme.colors.neutral400,
+            )
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/trashtrack/ui/shared/button/back/BackButton.kt
@@ -0,0 +1,56 @@
+package com.example.trashtrack.ui.shared.button.back
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.example.trashtrack.R
+
+@Composable
+fun BackButton(
+    modifier: Modifier = Modifier,
+    paddingTop: Dp = 0.dp,
+    paddingBottom: Dp = 0.dp,
+    paddingEnd: Dp = 0.dp,
+    paddingStart: Dp = 33.dp,
+    paddingIcon: Dp = 9.dp,
+    backButton: () -> Unit,
+    color: Color = Color(0xFFF6F6F5)
+) {
+    Box(
+        modifier = Modifier
+            .padding(
+                start = paddingStart,
+                top = paddingTop,
+                end = paddingEnd,
+                bottom = paddingBottom
+            )
+            .clickable(
+                onClick = backButton
+            )
+            .background(
+                color = color,
+                shape = RoundedCornerShape(6.dp)
+            ),
+
+        contentAlignment = Alignment.Center
+    ) {
+        Icon(
+            imageVector = ImageVector.vectorResource(R.drawable.ic_chevron_left),
+            contentDescription = null,
+            tint = Color.Black,
+            modifier = modifier
+                .padding(paddingIcon)
+        )
+    }
+}
